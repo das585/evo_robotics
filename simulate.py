8b760938a2e48c79314a2150cd7f5515b2dfba7e
David Smith <585das@gmail.com>
2022-01-20 19:30:26 -0500

    physics engine init

--- simulate.py
+++ simulate.py
@@ -1,1 +1,12 @@
-pass
+'''
+    David Smith
+    simulate.py: This program will be used to simulate the world that the robot will exist in as well as
+    the body of the robot eventually. Pybullet will be used as the physics engine for this world along 
+    with other provided files to help build the world.
+'''
+# import statements
+import pybullet as p
+
+# physics engine setup
+physicsClient = p.connect(p.GUI)
+p.disconnect
