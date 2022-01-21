cdb0dd5816b12d550bc5ac51aaf52d4fdbcf0c3d
David Smith <585das@gmail.com>
2022-01-20 20:16:55 -0500

    first run of generate.py

---
+++ generate.py
@@ -0,0 +1,16 @@
+'''
+    David Smith
+    generate.py: This program will be used to generate the body of any robots
+    created for this class using the provided pyrosim files
+'''
+
+# import statements
+import pyrosim.pyrosim as pyrosim
+
+# start the simulation
+pyrosim.Start_SDF("box.sdf")
+
+pyrosim.Send_Cube(name = "Box", post = [0, 0, 0.5], size = [1, 1, 1])
+
+pyrosim.End()
+
