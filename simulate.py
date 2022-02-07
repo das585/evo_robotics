0eef8b2a479c334c3626a6acc3224494eaa9b4a4
David Smith <585das@gmail.com>
2022-02-07 16:44:21 -0500

    added storage of sensor data

--- simulate.py
+++ simulate.py
@@ -5,6 +5,7 @@
     with other provided files to help build the world.
 '''
 # import statements
+import numpy as np
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
@@ -31,12 +32,24 @@ p.loadSDF("world.sdf")
 # prepare to use sensor values
 pyrosim.Prepare_To_Simulate(robotID)
 
+# numpy vector for storing sensor values
+backLegSensorValues = np.zeros(1000)
+
 # step through the world
 for x in range(1000):
+	
+	# step simulation
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    
+    # get touch value of back leg and add to vector
+    backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+     
+    # print the step number and wait 
+    print(x)
     time.sleep(.01)
+    
+#  save the vector
+np.save("data/sensors.npy", backLegSensorValues, allow_pickle=True, fix_imports=True)
 
 # physics engine disconnect
 p.disconnect
